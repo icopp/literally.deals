988b1e37b41d802aaef056ed6872120c65154ec1
Ian Copp <ian.copp@crowdstreet.com>
2020-03-13 16:15:50 -0700

    feat: improve CPU performance of progress bar rendering

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,39 +4,9 @@
 /**
  * @todo Submit to DefinitelyTyped.
  */
-declare module 'tiny-timer' {
-  import { EventEmitter } from 'events'
-
-  /**
-   * @event tick
-   * @event done
-   */
-  export default class Timer extends EventEmitter {
-    /** The current time in ms. */
-    readonly time: number
-
-    /** The total duration the timer is running for in ms. */
-    readonly duration: number
-
-    /** The current status of the timer as a string. */
-    readonly status: 'running' | 'paused' | 'stopped'
-
-    constructor(opts?: { interval?: number; stopwatch?: boolean })
-
-    /**
-     * Starts timer running.
-     * @param duration A duration specified in ms
-     * @param interval Optionally overide the default refresh interval in ms
-     */
-    start(duration: number, interval?: number): void
-
-    /** Stops timer. */
-    stop(): void
-
-    /** Pauses timer. */
-    pause(): void
-
-    /** Resumes timer. */
-    resume(): void
-  }
+declare module 'react-countdown-hook' {
+  export default function useCountDown(
+    timeToCount?: number,
+    interval?: number
+  ): [number, (timeToCount?: number) => void]
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
+import useInterval from '@use-it/interval'
 import format from 'format-duration'
 import he from 'he'
 import fetch from 'isomorphic-unfetch'
 import { GetServerSideProps, NextComponentType, NextPageContext } from 'next'
 import absoluteUrl from 'next-absolute-url'
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { FunctionComponent, useState } from 'react'
 import useSWR from 'swr'
 import { fetcherFn } from 'swr/dist/types'
-import Timer from 'tiny-timer'
 
 import FIVE_MINUTES_IN_MS from '~/constants/five-minutes-in-ms'
 import EbayItem from '~/typings/ebay-item'
@@ -14,6 +14,88 @@ import EbayItem from '~/typings/ebay-item'
 const fetcher: fetcherFn<EbayItem & { bigImageUrl: string | null }> = (
   url: string
 ) => fetch(url).then(r => r.json())
+
+const BuyButton: FunctionComponent<{ item: EbayItem }> = ({ item }) => (
+  <a
+    className="btn btn-lg btn-success"
+    href={item.viewItemURL}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Buy for ${item.sellingStatus.convertedCurrentPrice.toFixed(2)}{' '}
+    {item.shippingInfo.shippingServiceCost === 0 ? (
+      <>with free shipping</>
+    ) : (
+      <>plus ${item.shippingInfo.shippingServiceCost.toFixed(2)} shipping</>
+    )}
+  </a>
+)
+BuyButton.displayName = 'BuyButton'
+
+const ProgressBar: FunctionComponent<{
+  now: number
+  min: number
+  max: number
+}> = ({ now, min, max }) => (
+  <div className="progress">
+    <div
+      className="progress-bar progress-bar-striped bg-info"
+      role="progressbar"
+      aria-valuenow={now}
+      aria-valuemin={min}
+      aria-valuemax={max}
+      style={{
+        width: `${(now / max) * 100}%`
+      }}
+    />
+  </div>
+)
+ProgressBar.displayName = 'ProgressBar'
+
+const CardBody: FunctionComponent<{
+  item: EbayItem & { bigImageUrl: string | null }
+}> = ({ item }) => (
+  <div className="card-body">
+    <img
+      className="rounded mx-auto d-block"
+      src={item.bigImageUrl || item.galleryURL}
+    />
+    <div className="text-center my-2">
+      ({he.decode(item.primaryCategory.categoryName)})
+    </div>
+    <div className="text-center mt-4">
+      <BuyButton item={item} />
+    </div>
+  </div>
+)
+CardBody.displayName = 'CardBody'
+
+const CardFooter: FunctionComponent<{
+  now: number
+  min: number
+  max: number
+}> = ({ now, min, max }) => (
+  <div className="card-footer text-muted">
+    <div className="text-center">{format(max - now)} until the next item</div>
+    <ProgressBar now={now} min={min} max={max} />
+  </div>
+)
+CardFooter.displayName = 'CardBody'
+
+const FooterText: FunctionComponent<{}> = () => (
+  <div className="text-center mt-4">
+    (put together from a box of scraps by{' '}
+    <a
+      href="https://github.com/icopp"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      icopp
+    </a>
+    )
+  </div>
+)
+FooterText.displayName = 'FooterText'
 
 const Index: NextComponentType<
   NextPageContext,
@@ -24,92 +106,36 @@ const Index: NextComponentType<
     '/api/random-ebay-result',
     fetcher,
     {
-      initialData
+      initialData,
+      revalidateOnFocus: false
     }
   )
 
-  const [tick, setTick] = useState<number>(0)
-  const timer = useMemo(() => new Timer({ interval: 500, stopwatch: true }), [])
-  const finishTimer = useCallback(() => {
-    mutate()
-    timer.stop()
-    timer.start(FIVE_MINUTES_IN_MS)
-  }, [])
-  useEffect(() => {
-    timer.addListener('tick', setTick)
-    timer.addListener('done', finishTimer)
-    timer.start(FIVE_MINUTES_IN_MS)
-
-    return function(): void {
-      timer.removeListener('tick', setTick)
-      timer.removeListener('done', finishTimer)
-      timer.stop()
+  const [timePassed, setTimePassed] = useState(0)
+  useInterval(() => {
+    if (timePassed >= FIVE_MINUTES_IN_MS) {
+      mutate()
+      setTimePassed(0)
+      return
     }
-  }, [])
+
+    setTimePassed(timePassed => timePassed + 1000)
+  }, 1000)
 
   return (
     <div className="container">
       {data && (
         <div className="card my-4">
           <h3 className="card-header text-center">{he.decode(data.title)}</h3>
-          <div className="card-body">
-            <img
-              className="rounded mx-auto d-block"
-              src={data.bigImageUrl || data.galleryURL}
-            />
-            <div className="text-center my-2">
-              ({he.decode(data.primaryCategory.categoryName)})
-            </div>
-            <div className="text-center mt-4">
-              <a
-                className="btn btn-lg btn-success"
-                href={data.viewItemURL}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Buy for ${data.sellingStatus.convertedCurrentPrice.toFixed(2)}{' '}
-                {data.shippingInfo.shippingServiceCost === 0 ? (
-                  <>with free shipping</>
-                ) : (
-                  <>
-                    plus ${data.shippingInfo.shippingServiceCost.toFixed(2)}{' '}
-                    shipping
-                  </>
-                )}
-              </a>
-            </div>
-          </div>
-          <div className="card-footer text-muted">
-            <div className="text-center">
-              {format(FIVE_MINUTES_IN_MS - tick)} until the next item
-            </div>
-            <div className="progress">
-              <div
-                className="progress-bar progress-bar-striped progress-bar-animated bg-info"
-                role="progressbar"
-                aria-valuenow={tick}
-                aria-valuemin={0}
-                aria-valuemax={FIVE_MINUTES_IN_MS}
-                style={{ width: `${(tick / FIVE_MINUTES_IN_MS) * 100}%` }}
-              />
-            </div>
-          </div>
+          <CardBody item={data} />
+          <CardFooter now={timePassed} min={0} max={FIVE_MINUTES_IN_MS} />
         </div>
       )}
-      <div className="text-center mt-4">
-        (put together from a box of scraps by{' '}
-        <a
-          href="https://github.com/icopp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          icopp
-        </a>
-        )
-      </div>
+      <FooterText />
     </div>
   )
 }
+Index.displayName = 'Index'
 export default Index
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
