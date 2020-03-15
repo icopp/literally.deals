7be19da7f3ac1bd12cc1ce095e4686fbd51f6486
Ian Copp <copp.ian@gmail.com>
2020-03-15 14:31:52 -0700

    feat: change loading to happen entirely client-side for better rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import { css } from '@emotion/core'
 import useInterval from '@use-it/interval'
 import format from 'format-duration'
 import he from 'he'
 import fetch from 'isomorphic-unfetch'
-import { GetServerSideProps, NextComponentType, NextPageContext } from 'next'
-import absoluteUrl from 'next-absolute-url'
+import { NextComponentType } from 'next'
 import React, { FunctionComponent, useState } from 'react'
+import FadeLoader from 'react-spinners/FadeLoader'
+import SyncLoader from 'react-spinners/SyncLoader'
 import useSWR from 'swr'
 import { fetcherFn } from 'swr/dist/types'
 
@@ -15,98 +17,11 @@ const fetcher: fetcherFn<EbayItem & { bigImageUrl: string | null }> = (
   url: string
 ) => fetch(url).then(r => r.json())
 
-const BuyButton: FunctionComponent<{ item: EbayItem }> = ({ item }) => (
-  <a
-    className="btn btn-lg btn-success"
-    href={item.viewItemURL}
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Buy for ${item.sellingStatus.convertedCurrentPrice.toFixed(2)}{' '}
-    {item.shippingInfo.shippingServiceCost === 0 ? (
-      <>with free shipping</>
-    ) : (
-      <>plus ${item.shippingInfo.shippingServiceCost.toFixed(2)} shipping</>
-    )}
-  </a>
-)
-BuyButton.displayName = 'BuyButton'
-
-const ProgressBar: FunctionComponent<{
-  now: number
-  min: number
-  max: number
-}> = ({ now, min, max }) => (
-  <div className="progress">
-    <div
-      className="progress-bar progress-bar-striped bg-info"
-      role="progressbar"
-      aria-valuenow={now}
-      aria-valuemin={min}
-      aria-valuemax={max}
-      style={{
-        width: `${(now / max) * 100}%`
-      }}
-    />
-  </div>
-)
-ProgressBar.displayName = 'ProgressBar'
-
-const CardBody: FunctionComponent<{
-  item: EbayItem & { bigImageUrl: string | null }
-}> = ({ item }) => (
-  <div className="card-body">
-    <img
-      className="rounded mx-auto d-block"
-      src={item.bigImageUrl || item.galleryURL}
-    />
-    <div className="text-center my-2">
-      ({he.decode(item.primaryCategory.categoryName)})
-    </div>
-    <div className="text-center mt-4">
-      <BuyButton item={item} />
-    </div>
-  </div>
-)
-CardBody.displayName = 'CardBody'
-
-const CardFooter: FunctionComponent<{
-  now: number
-  min: number
-  max: number
-}> = ({ now, min, max }) => (
-  <div className="card-footer text-muted">
-    <div className="text-center">{format(max - now)} until the next item</div>
-    <ProgressBar now={now} min={min} max={max} />
-  </div>
-)
-CardFooter.displayName = 'CardBody'
-
-const FooterText: FunctionComponent<{}> = () => (
-  <div className="text-center mt-4">
-    (put together from a box of scraps by{' '}
-    <a
-      href="https://github.com/icopp"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      icopp
-    </a>
-    )
-  </div>
-)
-FooterText.displayName = 'FooterText'
-
-const Index: NextComponentType<
-  NextPageContext,
-  { initialData: EbayItem & { bigImageUrl: string | null } },
-  { initialData: EbayItem & { bigImageUrl: string | null } }
-> = ({ initialData }) => {
+const Index: NextComponentType = () => {
   const { data, mutate } = useSWR<EbayItem & { bigImageUrl: string | null }>(
     '/api/random-ebay-result',
     fetcher,
     {
-      initialData,
       revalidateOnFocus: false
     }
   )
@@ -122,27 +37,102 @@ const Index: NextComponentType<
     setTimePassed(timePassed => timePassed + 1000)
   }, 1000)
 
+  const SizedSyncLoader: FunctionComponent = () => <SyncLoader />
+
   return (
     <div className="container">
-      {data && (
-        <div className="card my-4">
-          <h3 className="card-header text-center">{he.decode(data.title)}</h3>
-          <CardBody item={data} />
-          <CardFooter now={timePassed} min={0} max={FIVE_MINUTES_IN_MS} />
+      <div className="card my-4">
+        <h3 className="card-header text-center">
+          {data ? he.decode(data.title) : <SizedSyncLoader />}
+        </h3>
+        <div className="card-body">
+          <div
+            className="rounded mx-auto d-flex"
+            style={{
+              backgroundColor: 'rgba(0, 0, 0, 0.027451)',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '15em',
+              width: '15em'
+            }}
+          >
+            {data ? (
+              <img
+                src={data.bigImageUrl || data.galleryURL}
+                className="rounded"
+                style={{ maxHeight: '100%', maxWidth: '100%' }}
+              />
+            ) : (
+              <FadeLoader />
+            )}
+          </div>
+          <div className="text-center my-2">
+            {data ? (
+              <>({he.decode(data.primaryCategory.categoryName)})</>
+            ) : (
+              <SizedSyncLoader
+                css={css`
+                  height: 1em;
+                `}
+              />
+            )}
+          </div>
+          <div className="text-center mt-4">
+            <a
+              className="btn btn-lg btn-success"
+              href={data ? data.viewItemURL : undefined}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {data ? (
+                <>
+                  Buy for ${data.sellingStatus.convertedCurrentPrice.toFixed(2)}{' '}
+                  {data.shippingInfo.shippingServiceCost === 0 ? (
+                    <>with free shipping</>
+                  ) : (
+                    <>
+                      plus ${data.shippingInfo.shippingServiceCost.toFixed(2)}{' '}
+                      shipping
+                    </>
+                  )}
+                </>
+              ) : (
+                <SizedSyncLoader />
+              )}
+            </a>
+          </div>
         </div>
-      )}
-      <FooterText />
+        <div className="card-footer text-muted">
+          <div className="text-center">
+            {format(FIVE_MINUTES_IN_MS - timePassed)} until the next item
+          </div>
+          <div className="progress">
+            <div
+              className="progress-bar progress-bar-striped bg-info"
+              role="progressbar"
+              aria-valuenow={timePassed}
+              aria-valuemin={0}
+              aria-valuemax={FIVE_MINUTES_IN_MS}
+              style={{
+                width: `${(timePassed / FIVE_MINUTES_IN_MS) * 100}%`
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="text-center mt-4">
+        (put together from a box of scraps by{' '}
+        <a
+          href="https://github.com/icopp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          icopp
+        </a>
+        )
+      </div>
     </div>
   )
 }
 Index.displayName = 'Index'
 export default Index
-
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const { origin } = absoluteUrl(req)
-  return {
-    props: {
-      initialData: await fetcher(`${origin}/api/random-ebay-result`)
-    }
-  }
-}
