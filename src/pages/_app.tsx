6bf44dd6ef9199625cbca99b434111561ecf4977
Ian Copp <copp.ian@gmail.com>
2020-03-15 14:32:29 -0700

    feat: pull site meta info from package.json instead of hardcoding

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import React, { FunctionComponent } from 'react'
 
+import packageJson from '~/../package.json'
+
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
   <>
     <Head>
@@ -14,32 +16,20 @@ const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
       />
       <meta name="theme-color" content="#ffffff" />
       <meta name="author" content="Ian Copp" />
-      <meta
-        name="description"
-        content={`"Those sure are some deals that exist."`}
-      />
+      <meta name="description" content={packageJson.description} />
       <meta name="image" content="/logo.png" />
-      <meta itemProp="name" content="literally.deals" />
-      <meta
-        itemProp="description"
-        content={`"Those sure are some deals that exist."`}
-      />
+      <meta itemProp="name" content={packageJson.name} />
+      <meta itemProp="description" content={packageJson.description} />
       <meta itemProp="image" content="/logo.png" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content="literally.deals" />
-      <meta
-        name="twitter:description"
-        content={`"Those sure are some deals that exist."`}
-      />
+      <meta name="twitter:title" content={packageJson.name} />
+      <meta name="twitter:description" content={packageJson.description} />
       <meta name="twitter:creator" content="@copp_ian" />
       <meta name="twitter:image:src" content="/logo.png" />
-      <meta name="og:title" content="literally.deals" />
-      <meta
-        name="og:description"
-        content={`"Those sure are some deals that exist."`}
-      />
+      <meta name="og:title" content={packageJson.name} />
+      <meta name="og:description" content={packageJson.description} />
       <meta name="og:url" content="https://literally.deals" />
-      <meta name="og:site_name" content="literally.deals" />
+      <meta name="og:site_name" content={packageJson.name} />
       <meta name="fb:admins" content="1340190059" />
       <meta name="fb:app_id" content="468200460029799" />
       <meta name="og:image" content="/logo.png" />
